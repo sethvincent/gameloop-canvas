1ba7cf61829be322efaab9f0c417392e50ecc906
sethvincent <sethvincent@gmail.com>
2015-12-04 17:40:33 -0500

    v0.3.3, update gameloop, standard, use budo

--- example.js
+++ example.js
@@ -1,18 +1,13 @@
-var Game = require('./index');
+var Game = require('./index')
 
 var game = Game({
-	width: 500,
-	height: 500
-});
+  width: 500,
+  height: 500
+})
 
-game.on('update', function(dt){
-	//console.log(dt);
-});
+game.on('draw', function (context) {
+  context.fillStyle = '#82d6e1'
+  context.fillRect(0, 0, game.width, game.height)
+})
 
-game.on('draw', function(context){
-	//console.log(context)
-	context.fillStyle = '#82d6e1';
-	context.fillRect(0, 0, game.width, game.height);
-});
-
-game.start();
+game.start()
--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var GameLoop = require('gameloop');
+var GameLoop = require('gameloop')
 
-module.exports = function(options){
+module.exports = function (options) {
   options || (options = {})
-  var canvas;
+  var canvas
 
-  if (!options.canvas){
-    canvas = document.createElement('canvas');
-    canvas.id = 'game';
-    document.body.appendChild(canvas);
-  } else if (typeof options.canvas === 'string'){
-    canvas = document.getElementById(options.canvas);
+  if (!options.canvas) {
+    canvas = document.createElement('canvas')
+    canvas.id = 'game'
+    document.body.appendChild(canvas)
+  } else if (typeof options.canvas === 'string') {
+    canvas = document.getElementById(options.canvas)
   } else if (typeof options.canvas === 'object' && options.canvas.tagName) {
     canvas = options.canvas
   }
 
   var game = new GameLoop({
     renderer: canvas.getContext('2d')
-  });
+  })
 
-  game.canvas = canvas;
-  game.width = canvas.width = options.width || 1024;
-  game.height = canvas.height = options.height || 480;
+  game.canvas = canvas
+  game.width = canvas.width = options.width || 1024
+  game.height = canvas.height = options.height || 480
 
-  return game;
+  return game
 }
